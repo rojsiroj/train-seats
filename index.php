11ffbc845ca15607faf84750ed9f2dc19bc44288
sirojudin <sirojudin145@gmail.com>
2019-12-12 17:53:54 +0700

    kai seats multiple selected

--- index.php
+++ index.php
@@ -5,6 +5,19 @@
 		print("<pre>".print_r($array, true)."</pre>");
 	}
 
+	$seatSelected = [];
+
+	if(isset($_GET['train'])){
+		$selected 		= $_GET['train'];
+		$wagonSelected 	= explode('-', $selected);
+		for ($i=0; $i < count($wagonSelected); $i++) { 
+			$arraySelected 	= explode('/', $wagonSelected[$i]);
+
+			$trainSelected[$i]	= $arraySelected[0];
+			$seatSelected[$i]	= $arraySelected[1];
+		}
+	}
+
 	$train_seats = file_get_contents('kai_seats.json');
 
 	$train_data  = json_decode($train_seats, true);
@@ -23,16 +36,6 @@
 			}
 		}
 	}
-
-	// echo "Gerbongnya";
-	// prettyArray($fieldGerbong);
-	// // echo "Seluruh Bangku";
-	// // prettyArray($fieldSeat);
-	// echo "Jumlah Bangku Kosong";
-	// prettyArray($seatAvailable);
-	// echo "Bangku Kosong";
-	// prettyArray($fieldSeatAvailable);
-	// prettyArray($fieldSeatStatus);
  ?>
 
  <!DOCTYPE html>
@@ -41,11 +44,11 @@
   <title>Tab Train Seat Example</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="bootstrap.min.css">
+  <script src="jquery.min.js"></script>
+  <script src="bootstrap.min.js"></script>
   <style type="text/css">
-  	.radio-toolbar.active input[type="radio"] {
+  	.radio-toolbar.active input[type="checkbox"] {
 	  display: none;
 	}
 
@@ -66,9 +69,23 @@
 	  cursor: not-allowed;
 	}
 
-	.radio-toolbar.active input[type="radio"]:checked+label {
+	/*.radio-toolbar.active input[type="checkbox"]:checked+label {
 	  background-color: #f58634;
 	  color: #fff;
+	}*/
+
+	.radio-checked+label {
+	  background-color: #f58634;
+	  color: #fff;
+	}
+
+	.label-checked {
+	  background-color: #f58634 !important;
+	  color: #fff;
+	}
+
+	.btn-save{
+		margin-top: 10px;
 	}
   </style>
 </head>
@@ -78,7 +95,16 @@
   <h2>Ganti Kursi</h2>
   <ul class="nav nav-tabs">
   	<?php foreach($fieldGerbong as $value) { 
-  		$gerbongActive = reset($fieldGerbong); ?>
+  		$gerbongActive = reset($fieldGerbong);
+
+  		if(isset($trainSelected)){
+  			$popular = array_count_values($trainSelected);
+			arsort($popular);
+			reset($popular);
+			$first_key = key($popular);
+  			$gerbongActive = $first_key;
+  		}
+  	?>
     	<li class="<?php echo ($value == $gerbongActive) ? 'active' : '';?>"><a class="tab-button" id="tab-<?php echo $value ?>" href="#seatList-<?php echo $value ?>">Gerbong <?php echo $value; ?></a></li>
   	<?php } ?>
   </ul>
@@ -91,18 +117,23 @@
 	      	<span class="seatColumn"><?php echo $seatColumn; ?></span>
 	      	<?php foreach ($seatRow as $seatKey => $seat) { ?>
 	      		<?php if(!empty($fieldSeatStatus[$key][$seat])){ ?>
-	      			<input type="radio" name="seat" value="" id="" disabled="disabled">
-  					<label for="" class="not-active">X</label>
+	      			<input type="checkbox" name="seat" value="" id="" disabled="disabled">
+  					<label for="" class="not-active"><?php echo $seat ?></label>
 	      		<?php }else{ ?>
-	      			<input type="radio" name="seat" value="<?php echo $seat ?>" id="<?php echo $key.'-'.$seat ?>">
+	      			<input type="checkbox" name="seat" value="<?php echo $seat ?>" id="<?php echo $key.'-'.$seat ?>" <?php echo in_array($key.'/'.$seat, $wagonSelected) ? "checked='checked'" : "";?> data-key="<?php echo $key;?>" class="single-checkbox <?php echo in_array($key.'/'.$seat, $wagonSelected) ? "radio-checked" : "";?>">
 
-  					<label for="<?php echo $key.'-'.$seat ?>" class="active"><?php echo $seat ?></label>
+  					<label for="<?php echo $key.'-'.$seat ?>" class="active <?php echo in_array($key.'/'.$seat, $wagonSelected) ? "label-checked" : "";?>"><?php echo $seat ?></label>
 	      		<?php } ?>
 	      	<?php } ?>
 	      	<br>
 	      <?php } ?>
 	    </div>
   	<?php } ?>
+  	<div class="row">
+  		<div class="col-lg-2 pull-right btn-save">
+			<input type="submit" name="" id="submit" class="btn btn-primary">
+  		</div>
+  	</div>
   </div>
 </div>
 
@@ -110,6 +141,46 @@
 	$('.tab-button').click(function () {
 	    $(this).tab('show');
 	});
+
+	$('#submit').click(function(){
+		var seat  = [];
+		var train = [];
+
+        $('.radio-checked').each(function(i){
+          seat[i] = $(this).val();
+          train[i] = $(this).data('key');
+        });
+		// for (var i = 0; i < $('input[name=seat]:checked').length; i++) {
+		// 	seat[i] = $('input[name=seat]:checked')[i].val()
+		// 	train[i] = $('input[name=seat]:checked')[i].data('key')
+		// }
+
+		// alert(train+"/"+seat);
+		console.log(train);
+		console.log(seat);
+	});
+
+
+	var limit = 3;
+	var allTabLimit = $('input.single-checkbox.radio-checked').length;
+
+	$('label.active').click(function(){
+		var id = $(this).attr('for');
+		if($(this).hasClass('label-checked')){
+			allTabLimit--;
+			$('input.single-checkbox#'+id).removeAttr('checked');
+			$('input.single-checkbox#'+id).removeClass('radio-checked');
+			$(this).removeClass('label-checked');
+		}else{
+			if(allTabLimit < limit){
+				allTabLimit++;
+				$('input.single-checkbox#'+id).attr('checked', 'checked');
+				$('input.single-checkbox#'+id).addClass('radio-checked');
+				$(this).addClass('label-checked');
+			}
+		}
+	})
+
 </script>
 
 </body>
